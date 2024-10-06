0af9fdbb5dcb7c7562be16585470744f1682fb11
Shaswata Ghosh <sonsghas909@gmail.com>
2024-10-07 01:48:42 +0530

    fourth commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,24 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import "../styles/login.css";
 
 const Login = () => {
+  const [signUpFlag, setSignUpFlag] = useState(false);
+  const handleSignUp = () => {
+    setSignUpFlag(!signUpFlag);
+  };
   return (
     <div className="login">
       <Header />
       <div className="login-card">
-        <div className="login-inner">
-          <span>Sign In</span>
+        <form className="login-inner">
+          <span>{signUpFlag ? "Sign Up" : "Sign In"}</span>
           <div className="input-container">
             <input placeholder="Enter Email or Mobile number" />
             <input placeholder="Enter Password" />
-            <button>Sign In</button>
+            <button>{signUpFlag ? "Sign Up" : "Sign In"}</button>
             <span className="forgot">Forgot password?</span>
           </div>
           <span className="new">
-            New to Netflix? <a style={{ fontWeight: "bold" }}>Sign Up now.</a>
+            {signUpFlag ? "Already a user?" : "New to Netflix?"}{" "}
+            <span
+              style={{ fontWeight: "bold", cursor: "pointer" }}
+              onClick={handleSignUp}
+            >
+              {signUpFlag ? "Sign Up now" : "Sign In now"}
+            </span>
           </span>
-        </div>
+        </form>
       </div>
     </div>
   );
